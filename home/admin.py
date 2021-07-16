13d08d0b152f7ee6bd80630b9a902b5801507ce1
Santosh Purbey <purbey.santosh@hotmail.com>
2021-07-17 02:35:12 +0545

    register models to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from home.models import App, Plan, Subscription
+
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'framework', 'user', 'created', 'modified')
+    search_fields = ('name', 'domain_name')
+    list_filter = ('created', 'modified')
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'created', 'modified')
+    search_fields = ('name',)
+    list_filter = ('created', 'modified')
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('app', 'plan', 'active', 'created', 'modified')
+    search_fields = ('app', 'plan')
+    list_filter = ('active', 'created', 'modified')
+
