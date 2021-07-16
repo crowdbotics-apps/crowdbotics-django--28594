4bae483c8c78f38ca1b0ba4c44ed7160c311d414
Santosh Purbey <purbey.santosh@hotmail.com>
2021-07-17 02:51:59 +0545

    fix typo in created_at, modified_at

--- home/admin.py
+++ home/admin.py
@@ -4,19 +4,19 @@ from home.models import App, Plan, Subscription
 
 @admin.register(App)
 class AppAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'framework', 'user', 'created_at', 'modified_at')
+    list_display = ('name', 'type', 'framework', 'user', 'created_at', 'updated_at')
     search_fields = ('name', 'domain_name')
-    list_filter = ('created_at', 'modified_at')
+    list_filter = ('created_at', 'updated_at')
 
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'created_at', 'modified_at')
+    list_display = ('name', 'description', 'price', 'created_at', 'updated_at')
     search_fields = ('name',)
-    list_filter = ('created_at', 'modified_at')
+    list_filter = ('created_at', 'updated_at')
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('app', 'plan', 'active', 'created_at', 'modified_at')
+    list_display = ('app', 'plan', 'active', 'created_at', 'updated_at')
     search_fields = ('app', 'plan')
-    list_filter = ('active', 'created_at', 'modified_at')
+    list_filter = ('active', 'created_at', 'updated_at')
 
